3c1d8afe0795117c98e69497e2109c0346b3f124
lexinor <alternonolimit@gmail.com>
2020-03-21 21:50:25 +0100

    Created custom view

--- exoCustomComp/app/src/main/java/com/alessandro/exocustomcomp/customView/CustomComponent.kt
+++ exoCustomComp/app/src/main/java/com/alessandro/exocustomcomp/customView/CustomComponent.kt
@@ -1,33 +1,33 @@
 package com.alessandro.exocustomcomp.customView
 
 import android.content.Context
-import android.graphics.Canvas
 import android.graphics.Color
 import android.util.AttributeSet
 import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
-import android.widget.TextView
-import kotlinx.android.synthetic.*
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.alessandro.exocustomcomp.R
 
-class CustomComponent(context: Context, attrs: AttributeSet): View(context, attrs) {
+class CustomComponent(context: Context, attrs: AttributeSet): ConstraintLayout(context, attrs) {
 
-    private val src: String = ""
-    private val text: String = ""
-    private val textColor = Color.GRAY
-
-    //private val textView: TextView
-
+    private var editText: EditText
+    private var imgView: ImageView
 
     init {
-        SetupAttributes(attrs)
+        View.inflate(context, R.layout.custom_component, this)
+
+        editText = findViewById(R.id.textView)
+        imgView = findViewById(R.id.imgView)
+
+        context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.CustomComponent,
+            0, 0).apply {
+                editText.setText(getString(R.styleable.CustomComponent_text))
+                editText.setTextColor(getColor(R.styleable.CustomComponent_textColor, Color.GRAY))
+                imgView.setImageResource(getResourceId(R.styleable.CustomComponent_src, R.drawable.pepe))
+        }.recycle()
+
     }
-
-
-
-    private fun SetupAttributes(attrs: AttributeSet?){
-        val typedArray = context.theme.obtainStyledAttributes(attrs,R.styleable.clearFindViewByIdCache())
-    }
-
-
 }
